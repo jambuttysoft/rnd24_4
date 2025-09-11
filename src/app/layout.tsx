0f69cfc098ac2fce70fd3a70bab911aafade9349
Karim Al-Sherif <stronciy@gmail.com>
2025-09-11 11:36:01 +0300

    fix title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "@/components/theme-provicer";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  title: "Normal Human",
+  title: "jambutty RnD 4 - 2024",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
